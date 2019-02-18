e50704869583e394bdc842132c0e0cb69d7eb82b
Luc Engelen <luc@kabisa.nl>
2019-02-18 09:33:32 +0100

    Add bean for REST template

--- src/main/java/nl/kabisa/rpc/rest/ProtobufConfiguration.java
+++ src/main/java/nl/kabisa/rpc/rest/ProtobufConfiguration.java
@@ -3,12 +3,21 @@ package nl.kabisa.rpc.rest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class ProtobufMessageConverterConfiguration {
+public class ProtobufConfiguration {
 
     @Bean
     ProtobufHttpMessageConverter protobufHttpMessageConverter() {
         return new ProtobufHttpMessageConverter();
+    }
+
+    @Bean
+    RestTemplate restTemplate(ProtobufHttpMessageConverter protobufHttpMessageConverter) {
+        var restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(protobufHttpMessageConverter);
+
+        return restTemplate;
     }
 }
