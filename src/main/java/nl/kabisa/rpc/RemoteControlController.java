e08e34e77de2465e0f8f9e353a87eb781cb49413
Luc Engelen <luc@kabisa.nl>
2019-02-18 09:34:20 +0100

    Add REST API for remote control

---
+++ src/main/java/nl/kabisa/rpc/RemoteControlController.java
@@ -0,0 +1,102 @@
+package nl.kabisa.rpc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import nl.kabisa.rpc.proto.VectorsServiceConsumer;
+import nl.kabisa.rpc.rest.VectorControllerConsumer;
+
+@RestController
+public class RemoteControlController {
+
+    private final VectorControllerConsumer vectorControllerConsumer;
+    private final VectorsServiceConsumer vectorsServiceConsumer;
+
+    @Autowired
+    public RemoteControlController(VectorControllerConsumer vectorControllerConsumer, VectorsServiceConsumer vectorsServiceConsumer) {
+        this.vectorControllerConsumer = vectorControllerConsumer;
+        this.vectorsServiceConsumer = vectorsServiceConsumer;
+    }
+
+    private static class Statistics {
+
+        private final String hostname;
+        private final int port;
+        private final int numberOfIterations;
+        private final int numberOfVectors;
+        private final long duration;
+
+        public Statistics(String hostname, int port, int numberOfIterations, int numberOfVectors, long duration) {
+            this.hostname = hostname;
+            this.port = port;
+            this.numberOfIterations = numberOfIterations;
+            this.numberOfVectors = numberOfVectors;
+            this.duration = duration;
+        }
+
+        public String getHostname() {
+            return hostname;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public int getNumberOfIterations() {
+            return numberOfIterations;
+        }
+
+        public int getNumberOfVectors() {
+            return numberOfVectors;
+        }
+
+        public long getDuration() {
+            return duration;
+        }
+    }
+
+    @GetMapping("/remote/rest/json/vectors")
+    public Statistics getRestJsonVectors(
+            @RequestParam String hostname, @RequestParam int port, @RequestParam int numberOfIterations, @RequestParam long seed, @RequestParam int numberOfVectors) {
+
+        var startTime = System.currentTimeMillis();
+
+        for (var i = 0; i < numberOfIterations; ++i) {
+            vectorControllerConsumer.getJsonVectors(hostname, port, seed, numberOfVectors);
+        }
+
+        var duration = System.currentTimeMillis() - startTime;
+
+        return new Statistics(hostname, port, numberOfIterations, numberOfVectors, duration);
+    }
+
+    @GetMapping("/remote/rest/proto/vectors")
+    public Statistics getRestProtoVectors(
+            @RequestParam String hostname, @RequestParam int port, @RequestParam int numberOfIterations, @RequestParam long seed, @RequestParam int numberOfVectors) {
+
+        var startTime = System.currentTimeMillis();
+
+        for (var i = 0; i < numberOfIterations; ++i) {
+            vectorControllerConsumer.getProtoVectors(hostname, port, seed, numberOfVectors);
+        }
+
+        var duration = System.currentTimeMillis() - startTime;
+
+        return new Statistics(hostname, port, numberOfIterations, numberOfVectors, duration);
+    }
+
+    @GetMapping("/remote/proto/vectors")
+    public Statistics getProtoVectors(
+            @RequestParam String hostname, @RequestParam int port, @RequestParam int numberOfIterations, @RequestParam long seed, @RequestParam int numberOfVectors) {
+
+        var startTime = System.currentTimeMillis();
+
+        for (var i = 0; i < numberOfIterations; ++i) {
+            vectorsServiceConsumer.getVectors(hostname, port, seed, numberOfVectors);
+        }
+
+        var duration = System.currentTimeMillis() - startTime;
+
+        return new Statistics(hostname, port, numberOfIterations, numberOfVectors, duration);
+    }
+}
